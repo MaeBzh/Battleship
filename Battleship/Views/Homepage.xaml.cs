4584b68f6b41025fb4f09c5046c2c0fa0ac3eafd
yannik <yguillodo@gmail.com>
2019-02-07 19:18:56 +0100

    formulaire lancement

---
+++ Battleship/Views/Homepage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Battleship.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour Homepage.xaml
+    /// </summary>
+    public partial class Homepage : Window
+    {
+        public Homepage()
+        {
+            InitializeComponent();
+            this.Content = new Settings();
+
+        }
+    }
+}
---
+++ Battleship/Views/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Battleship.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void GoTN_Click(object sender, RoutedEventArgs e)
+        {
+            //Settings s1 = new Settings();
+            //this.Content = s1;
+            Homepage h1 = new Homepage();
+            h1.Show();
+
+        }
+    }
+
+}
